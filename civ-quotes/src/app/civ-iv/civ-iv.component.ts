d65238cccec8857ba07d2eff38f205761d391274
benhans0n <benhanson784@gmail.com>
2021-04-09 14:08:57 -0500

    Civ IV done

--- civ-quotes/src/app/civ-iv/civ-iv.component.ts
+++ civ-quotes/src/app/civ-iv/civ-iv.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from './civivquote';
 
 @Component({
   selector: 'app-civ-iv',
@@ -6,8 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./civ-iv.component.css']
 })
 export class CivIvComponent implements OnInit {
+  aQuotes = new Array<Quote>();
+  cQuotes = new Array<Quote>();
+  mQuotes = new Array<Quote>();
+  rQuotes = new Array<Quote>();
+  iQuotes = new Array<Quote>();
+  moQuotes = new Array<Quote>();
 
-  constructor() { }
+  constructor() {
+    let holdQuotes = new Array<Quote>();
+    holdQuotes = require('./civ-iv-quotes.json').quotes;
+
+    this.aQuotes = holdQuotes.filter(quote => quote.era === "a");
+    this.cQuotes = holdQuotes.filter(quote => quote.era === "c");
+    this.mQuotes = holdQuotes.filter(quote => quote.era === "m");
+    this.rQuotes = holdQuotes.filter(quote => quote.era === "r");
+    this.iQuotes = holdQuotes.filter(quote => quote.era === "i");
+    this.moQuotes = holdQuotes.filter(quote => quote.era === "mo");
+   }
 
   ngOnInit(): void {
   }
