455c81ab985bdfe2731e0b57496f27d0b8b5cc27
benhans0n <benhanson784@gmail.com>
2021-06-24 23:20:55 -0500

    Added titles for tab bar and SEO

--- civ-quotes/src/app/app.module.ts
+++ civ-quotes/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, Title } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { RouterModule } from '@angular/router';
@@ -47,7 +47,9 @@ import { CivViComponent } from './civ-vi/civ-vi.component';
     MatDividerModule,
     MatCardModule
   ],
-  providers: [],
+  providers: [
+    Title
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
